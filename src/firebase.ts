cf21e0d2757b8c85ee633ac22626acdc292dff6a
for7raid <alexey@for7raid.ru>
2021-10-13 12:15:11 +0300

    auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +18,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 console.log('fire app init')
+
+
+const auth = getAuth();
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    signInWithPopup(auth, new GoogleAuthProvider());
+  }
+})
+
 export default { fireapp: app };
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="logout">Выйти</button>
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+
+
+import { defineComponent, onMounted } from "vue";
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+export default defineComponent({
+  name: "Login",
+  setup() {
+    debugger;
+    //  const app = firebase.getApp();
+    //  const auth = getAuth(app);
+
+    // const ui = new firebaseui.auth.AuthUI(auth);
+
+    const logout = async () => {
+      //await auth.signOut();
+    };
+
+    onMounted(() => {
+      // ui.start("#firebaseui-auth-container", {
+      //   signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+      // });
+    });
+
+    return {
+      logout,
+    };
+  },
+});
+</script>
